86b4d508f06e25e5ac58b4887be4f96cfd32f4ec
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2025-01-01 17:08:31 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,29 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
 from streamlit_gsheets import GSheetsConnection
 from datetime import datetime
+import pytz
 
 # Streamlit 페이지 설정
 st.set_page_config(page_title="행사 신청", page_icon="📃")
-st.title("📃 행사 신청 사이트")
-st.info("골든래빗고 행사 신청 사이트입니다. 아래의 양식에 맞게 기입하여 '제출'버튼을 눌러주세요. 현재시간까지 신청 현황은 아래에서 확인할 수 있습니다.")
+st.title("📃 디지털 기반 업무 적용 실천하기!")
+st.info("**동계 1정 연수 : 디지털 기반 업무의 실제 강의**를 들으신 후, 아이디어를 공유하는 장입니다. 선생님들의 창의적인 아이디어를 서로 공유해주세요! 다른 선생님들의 아이디어를 보고 개선시키는 것도 가능합니다:)")
+
+# 구글 폼 삽입
+iframe_url = 'https://forms.gle/6MCgNgMEQXRPkW9v8'
+st.components.v1.iframe(src=iframe_url, width=None, height=500, scrolling=True)
 
 # 신청자 현황 대시보드
-st.subheader("신청자 현황 대시보드")
-st.write("현재까지 신청자 현황입니다. 신청 시 참고해주세요.")
-
+st.subheader("공유된 응답 대시보드")
 # 구글 시트 연결
 conn = st.connection("gsheets", type=GSheetsConnection)
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-st.write(f"**현재 시간:** {current_time}")
+
+# 한국 시간 (KST) 설정
+kst = pytz.timezone("Asia/Seoul")
+current_time_kst = datetime.now(kst).strftime("%Y-%m-%d %H:%M:%S")
 
 # 새로고침 버튼 추가
 if st.button("🔄 데이터 새로고침"):
@@ -24,13 +32,31 @@ if st.button("🔄 데이터 새로고침"):
 # 데이터 읽기
 df = conn.read()
 
+
 # 신청 날짜 데이터 처리 및 시각화
 if not df.empty:
-    dates = df['신청 날짜'].str.split(', ').explode()
-    st.bar_chart(dates.value_counts())
+    data = df.iloc[:, 1].str.split(', ').explode()
+    response_count = data.value_counts()
+    
+    # 응답자 총합
+    total_respondents = len(df)
+    st.write(f"**응답자 총합:** {total_respondents}명, **현재 시간:** {current_time_kst}")
+    
+    # Matplotlib을 이용한 원그래프
+    # st.subheader("응답자 현황: 원그래프")
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.pie(
+        response_count.values, 
+        labels=response_count.index, 
+        autopct='%1.1f%%', 
+        startangle=90, 
+        colors=sns.color_palette("pastel"),
+        textprops={'fontsize': 12}
+    )
+    # ax.set_title("응답자 현황", fontsize=16, fontweight="bold")
+    
+    # Streamlit에 그래프 표시
+    st.pyplot(fig)
 else:
     st.warning("현재 신청 데이터가 없습니다.")
-
-# 구글 폼 삽입
-iframe_url = 'https://forms.gle/tqcE1P23xVwPm1LT9'
-st.components.v1.iframe(src=iframe_url, width=None, height=500, scrolling=True)
